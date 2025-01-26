9b3c023f94d510b50477c5233644a9a55f81159c
amuhammed005 <amuhammed005@st.ug.edu.gh>
2025-01-26 22:10:30 +0000

    Shopping cart program modified

--- shopping_cart_program.py
+++ shopping_cart_program.py
@@ -4,20 +4,27 @@ foods = []
 prices = []
 cart_total = 0
 while True:
-    food = input("Enter a food to buy (q to quit): ").lower()
+    food = input("Enter a food to buy (q to quit): ").strip().lower()
     if food == "q":
         break
-    else:
-        price = float(input(f"Enter the price of a {food}: $"))
-        foods.append(food)
-        prices.append(price)
 
-print("------------ Your Cart -----------")
-for food in foods:
-    print(food, end=" ")
+    while True:
+        price = input(f"Enter the price of {food}: $").strip()
+        try:
+            price = float(price);
+            if price  < 0:
+                print("Price can not be negative. Please enter a valid price")
+            else:
+                break
+        except ValueError:
+            print("Invalid input. Enter a valid number")
+    
+    foods.append(food)
+    prices.append(price)
 
-for price in prices:
-    cart_total += price
+print("\n------------ Your Cart -----------")
+for food, price in zip(foods, prices):
+    print(f"{food.capitalize()}:  ${price:.2f}")
 
-print()
-print(f"Your total is ${cart_total}")
+cart_total = sum(prices)
+print(f"\nYour total is ${cart_total:.2f}")
