484b6ac42a91f47ee7b8ccb6270bbaac9527a0ec
amuhammed005 <amuhammed005@st.ug.edu.gh>
2025-01-26 14:54:44 +0000

    Make code efficient

--- compound_interest_calculator.py
+++ compound_interest_calculator.py
@@ -1,26 +1,29 @@
-total_amount = 0
 principle = 0
 rate = 0
 time = 0
 
-while principle <= 0:
+while True:
     principle = float(input("Enter the principle amount: "))
-    if principle <= 0:
+    if principle < 0:
         print("Principle can't be less than or equal to zero")
+    else:
+        break;
 
-
-while rate <= 0:
+while True:
     rate = float(input("Enter the interest rate: "))
-    if rate <= 0:
+    if rate < 0:
         print("Interest rate can't be less than or equal to zero")
+    else:
+        break;
 
-
-while time <= 0:
+while True:
     time = int(input("Enter the time in years: "))
     if time <= 0:
         print("Time can't be less than or equal to zero")
+    else:
+        break;
 
-print(f"Principle: {principle}, Rate: {rate}, Time: {time}")
+# print(f"Principle: {principle}, Rate: {rate}, Time: {time}")
 
 total_amount = principle * pow(1 + (rate / 100), time)
 print(f"Your balance after {time} year/s is: ${total_amount:.2f}")
